eebd041e58bc8e3dc021915715ba2aee8bbbccac
John Doe <john.doe@gmail.com>
2019-07-10 10:22:52 -0300

    Trabalhando com Arrays Lineares

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,7 +11,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git");
+            /*
+            string[] nameList = new string[2] { "Valor1", "Valor2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+            */
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+                Console.WriteLine(dateList[i]);
+                //dateList[i] = DateTime.Now.ToString("hh:mm:ss:ffff");
+                //Thread.Sleep(1000);
+            }
+            
+            foreach (var item in dateList)
+                Console.WriteLine(item);            
+
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+
+            Console.ReadKey();
         }
     }
 }
