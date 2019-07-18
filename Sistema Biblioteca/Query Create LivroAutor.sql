a2fcd76c49ee0f964823d6ab9857bdb4b2eade35
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 17:24:46 -0300

    Criando as tabelas para a Biblioteca

---
+++ Sistema Biblioteca/Query Create LivroAutor.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LivroAutor]
+(
+    [Livro] INT NOT NULL, 
+    [Autor] INT NOT NULL, 
+    CONSTRAINT [FK_LivroAutor_To_Livros] FOREIGN KEY ([Livro]) REFERENCES [Livros]([Id]),
+	CONSTRAINT [FK_LivroAutor_To_Autores] FOREIGN KEY ([Autor]) REFERENCES [Autores]([Id])
+)
---
+++ Sistema Biblioteca/Query Create Livros.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Livros]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+	[Registro] INT NOT NULL,
+    [Titulo] VARCHAR(1200) NOT NULL, 
+    [Isbn] VARCHAR(15) NOT NULL, 
+    [Genero] INT NOT NULL, 
+    [Editora] INT NOT NULL, 
+    [Sinopse] NVARCHAR(MAX) NULL,
+	[Observacoes] VARCHAR(1000) NULL,
+	[Ativo] BIT NOT NULL,
+	[UsuInc] INT NOT NULL,
+	[UsuAlt] INT NOT NULL,
+	[DatInc] DATETIME NOT NULL DEFAULT GETDATE(),
+	[DatAlt] DATETIME NOT NULL DEFAULT GETDATE(), 
+    CONSTRAINT [FK_Livros_To_Generos] FOREIGN KEY ([Genero]) REFERENCES [Generos]([Id]), 
+    CONSTRAINT [FK_Livros_To_Editoras] FOREIGN KEY ([Editora]) REFERENCES [Editoras]([Id]), 
+    CONSTRAINT [FK_Livros_To_UsuariosInc] FOREIGN KEY ([UsuInc]) REFERENCES [Usuarios]([Id]),
+	CONSTRAINT [FK_Livros_To_UsuariosAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [Usuarios]([Id])
+)
