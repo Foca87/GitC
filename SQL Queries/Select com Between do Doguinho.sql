26fbfd93fc86d3192a83039c04797e117d66672a
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:09:06 -0300

    SELECT com BETWEEN

---
+++ SQL Queries/Select com Between do Doguinho.sql
@@ -0,0 +1,8 @@
+﻿select
+	Tipo,
+	SUM(TotalVendas) as 'Total Vendas',
+	MONTH(DataVenda) as 'Mês'
+from FoodTruckZezinho
+where MONTH(DataVenda) BETWEEN 1 and 3
+group by Tipo, MONTH(DataVenda)
+order by [Total Vendas] desc
