4fa6a15712fe3f9dfbaefaff2c3c35cc0b5b197d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-26 15:20:25 -0300

    Projeto da Calculadora com DLL importada.

---
+++ BibliotecaDeOperacoesDaCalculadora/BibliotecaDeOperacoesDaCalculadora/OperacoesCalculadora.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeOperacoesDaCalculadora
+{
+    public class OperacoesCalculadora
+    {
+        public class OperacoesAritmeticas
+        {
+
+            /// <summary>
+            /// Faz a soma de dois valores (x e y).
+            /// </summary>
+            /// <param name="pX">Primeiro valor. Aceita float.</param>
+            /// <param name="pY">Segundo valor. Aceita float.</param>
+            /// <returns>Retorna a soma de ambos os valores informados em float.</returns>
+            public float Adicao(float pX, float pY)
+            {
+                return pX + pY;
+            }
+
+            /// <summary>
+            /// Faz a subtração de doís valores, seguindo a ordem informada (x e y).
+            /// </summary>
+            /// <param name="pX">Valor float de onde será subtraído.</param>
+            /// <param name="pY">Valor float que irá subtrair.</param>
+            /// <returns>Retorna o resultado da subtração em float.</returns>
+            public float Subtracao(float pX, float pY)
+            {
+                return pX - pY;
+            }
+
+            /// <summary>
+            /// Multiplica dois valores (x e y).
+            /// </summary>
+            /// <param name="pX">Primeiro valor. Aceita até float.</param>
+            /// <param name="pY">Segundo valor. Aceita até float.</param>
+            /// <returns>Retorna o resultado da multiplicação em float.</returns>
+            public float Multiplicacao(float pX, float pY)
+            {
+                return pX * pY;
+            }
+
+            /// <summary>
+            /// Faz a divisão de doís valores, seguindo a ordem informada (x e y).
+            /// </summary>
+            /// <param name="pX">Valor float que será o dividendo.</param>
+            /// <param name="pY">Valor float que será o divisor.</param>
+            /// <returns>Retorna o resultado da divisão em float.</returns>
+            public float Divisao(float pX, float pY)
+            {
+                return pX / pY;
+            }
+
+            /// <summary>
+            /// Calcula a área de um retângulo.
+            /// </summary>
+            /// <param name="pX">Informe o valor da base do retângulo (float).</param>
+            /// <param name="pY">Informe o valor da altura do retângulo (float).</param>
+            /// <returns>Retorna o resultado da multiplicação em float.</returns>
+            public float AreaRetangulo(float pX, float pY)
+            {
+                return pX * pY;
+            }
+
+            /// <summary>
+            /// Calcula a área de um triângulo equilátero.
+            /// </summary>
+            /// <param name="pBase">Informe o valor da base do triângulo (float).</param>
+            /// <param name="pAltura">Informe o valor da altura do triângulo (float).</param>
+            /// <returns>Retorna um float com o valor da área do triângulo.</returns>
+            public float AreaTriangulo (float pBase, float pAltura)
+            {
+                return (pBase * pAltura) / 2;
+            }
+
+            /// <summary>
+            /// Calcula o raio de um Círculo utilizando da área do mesmo.
+            /// </summary>
+            /// <param name="pArea">Informe a área do círculo (float).</param>
+            /// <returns>Retorna um double com o valor do raio do círculo.</returns>
+            public double RaioDoCirculo(float pArea)
+            {
+                return Math.Sqrt(pArea / Math.PI);
+            }
+        }
+    }
+}
---
+++ ProjetoCalculadoraComClasseImportada/ProjetoCalculadoraComClasseImportada/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculadoraClass;
+
+namespace ProjetoCalculadoraComClasseImportada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular calculadora = new Calcular();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("----------Calculadora----------");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Calcular Área de Retângulo");
+                Console.WriteLine("6 - Calcular Área de Triângulo");
+                Console.WriteLine("7 - Calcular o Raio do Círculo");
+                Console.WriteLine("0 - Fechar o programa");
+                Console.WriteLine("Informe a opção desejada");
+
+                switch (Console.ReadKey().KeyChar.ToString())
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("\r\nDigite o primeiro valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o segundo valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine(string.Format("{0} + {1} = {2}", valorX, valorY, calculadora.Somar(valorX, valorY)));
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "2":
+                        {
+                            Console.WriteLine("\r\nDigite o primeiro valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o segundo valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine(string.Format("{0} - {1} = {2}", valorX, valorY, calculadora.Subtrair(valorX, valorY)));
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "3":
+                        {
+                            Console.WriteLine("\r\nDigite o primeiro valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o segundo valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine(string.Format("{0} * {1} = {2}", valorX, valorY, calculadora.Multiplicar(valorX, valorY)));
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "4":
+                        {
+                            Console.WriteLine("\r\nDigite o primeiro valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o segundo valor: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine(string.Format("{0} + {1} = {2}", valorX, valorY, calculadora.Dividir(valorX, valorY)));
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "5":
+                        {
+                            Console.WriteLine("\r\nDigite o valor da base do retângulo: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o valor da altura do retângulo: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine($"A área do retângulo é igual a {calculadora.CalcularAreaRetangulo(valorX,valorY)}");
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "6":
+                        {
+                            Console.WriteLine("\r\nDigite o valor da base do triângulo: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+                            Console.WriteLine("Digite o valor da altura do triângulo: ");
+                            double.TryParse(Console.ReadLine(), out double valorY);
+
+                            Console.WriteLine($"A área do triângulo é igual a {calculadora.CalcularAreaTriangulo(valorX, valorY)}");
+
+                            Console.ReadKey();
+
+                        } break;
+                    case "7":
+                        {
+                            Console.WriteLine("\r\nDigite o valor da área do círculo: ");
+                            double.TryParse(Console.ReadLine(), out double valorX);
+
+                            Console.WriteLine($"O valor do raio do círculo é igual a {calculadora.CalcularRaioCirculo(valorX)}");
+
+                            Console.ReadKey();
+
+                        } break;
+
+                    case "0": { return; }
+                    default: { Console.WriteLine("\r\nOpção incorreta! Por favor escolha novamente."); } break;
+                }
+
+            } while (true);
+        }
+    }
+}
