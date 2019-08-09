b18b0868da9c4b76923b8e2a5596f5d7b1734054
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-09 14:27:41 -0300

    Upload dos códigos

---
+++ UsandoRegEx/UsandoRegEx/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsandoRegEx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            var nomeRegex = "^[A-Z][a-z]*(\\s[A-Z][a-z]*)+$";
+            
+            var telefoneRegex = @"^\++[1-9]{1,2}\ (\([1-9]{2}\))\s([9]{1})?([0-9]{4})-([0-9]{4})$";
+
+            var emailRegex = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+            bool campoNome = Regex.IsMatch(tbxNome.Text, nomeRegex, RegexOptions.None);
+            bool campoTelefone = Regex.IsMatch(tbxTelefone.Text, telefoneRegex, RegexOptions.None);
+            bool campoEmail = Regex.IsMatch(tbxEmail.Text, emailRegex, RegexOptions.IgnoreCase);
+
+
+            if (campoNome && campoTelefone && campoEmail)
+                MessageBox.Show("Cadastro salvo com sucesso!");
+
+            if (!campoNome)
+                MessageBox.Show("O nome informado não é válido!");
+
+            if (!campoTelefone)
+                MessageBox.Show("O telefone informado não é válido!");
+
+            if (!campoEmail)
+                MessageBox.Show("O e-mail informado não é válido!");
+        }
+    }
+}
---
+++ WpfAppEntity/WpfAppEntity/Views/ucLogin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppEntity.Data;
+
+namespace WpfAppEntity.Views
+{
+    /// <summary>
+    /// Interaction logic for ucLogin.xaml
+    /// </summary>
+    public partial class ucLogin : UserControl
+    {
+        public ucLogin()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler success;
+        public event EventHandler fail;
+
+        public BibliotecaDBContex context = new BibliotecaDBContex();
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = tbxLogin.Text;
+            string userPass = tbxSenha.Password;
+
+            var result = context.Usuarios.FirstOrDefault(x => x.Login == userName && x.Senha == userPass);
+
+            if (result?.Id > 0)
+                success("Usuario logado com sucesso!", new EventArgs());
+            else
+                fail($"Falha ao logar com usuário {userName}.", new EventArgs());
+        }
+    }
+}
